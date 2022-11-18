6f4f72648a0ff07e3edbdeea45409ec061c18e77
AlejoMora991014 <alejandroester1910@gmail.com>
2022-11-17 19:44:08 -0600

    conectando desde el programa a un servidor de sql server

--- Program.cs
+++ Program.cs
@@ -5,8 +5,26 @@ using proyectoEF.Contexto;
 var builder = WebApplication.CreateBuilder(args);
 
 //antes de contruir la app vamos a preparar el entorno de EF
+//ahora vamos a hacer la conexion a una BD de sql server local 
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>("Data Source=BARTOMORA\\SQLEXPRESS;Initial Catalog=TareasDb;user id=username;password=pass;TrustServerCertificate=True");
+//recordar que solo puede haber una configuracion de services por cada contexto.
+
+#region comentarioParametros
+    /*
+        Parametros:
+            Data source
+                indica el string de conexion
+            Initial Catalog
+                Indica el nombre de nuestra BD
+            id; password
+                credenciales para el servicio de BD
+            TrustServerCertificate
+                esto es para indicar que el Data source es seguro en caso que no lo acepte
+            Recordar que esto son malas practicas
+
+    */
+#endregion termina comentarioParametros
 
 
 var app = builder.Build();
@@ -17,7 +35,10 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+    //aca deberia tirar error poque ya no tenemos la BD en memoria pero si en el servidor de sql 
+    //es decir podriamos ver la BD en el servidor sql server
+    return Results.Ok("Base de datos en memoria?: " + dbContext.Database.IsInMemory());
 
 });
 
