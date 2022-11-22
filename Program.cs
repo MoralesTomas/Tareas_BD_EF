71a6f677fad040d36b0d6ee038d781edeea05e5d
AlejoMora991014 <alejandroester1910@gmail.com>
2022-11-22 16:18:31 -0600

    terminando CRUD

--- Program.cs
+++ Program.cs
@@ -67,4 +67,41 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
     return Results.Ok();   
 });
 
+
+//para actualizar datos
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea,[FromRoute] Guid id)=>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual!=null)
+    {
+        tareaActual.CategoriaID = tarea.CategoriaID;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+
+    }
+
+    return Results.NotFound();   
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual!=null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
 app.Run();
